940c5e14be817722b600f14d2a78e47f3417e6f4
Benson <472756921@qq.com>
2018-01-29 11:46:54 +0800

    change

--- src/components/imgCat.vue
+++ src/components/imgCat.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="imageCat">
-      <Button class="getMessage"  @click="getMessage" type="primary" icon="images">图片上传</Button>
+      <Button class="getMessage"  @click="getMessage" type="primary" icon="images">选择图片</Button>
       <input type="file" id="fileElem" multiple accept="image/*" style="display:none" @change="handleFiles()">
       <br/>
       <br/>
@@ -23,115 +23,118 @@
 </template>
 
 <script>
-    export default {
-      name: "img-cat",
-      data() {
-        return {
-          img: '',
-        }
-      },
-      mounted(e) {
-        let content = document.getElementById('content');
-        let imgAc = document.getElementById('imgAc');
-        let iamgeShow = document.getElementById('iamgeShow');
-        let sendImgShow = document.getElementById('sendImgShow');
-        let dv = document.getElementById('kk');
+  export default {
+    name: "img-cat",
+    data() {
+      return {
+        img: '',
+      }
+    },
+    mounted(e) {
+      let content = document.getElementById('content');
+      let imgAc = document.getElementById('imgAc');
+      let iamgeShow = document.getElementById('iamgeShow');
+      let sendImgShow = document.getElementById('sendImgShow');
+      let dv = document.getElementById('kk');
 
-        let x = 0, y = 0, l = 0, t = 0, maxH = 0, maxW = 0;
-        let isDown = false;
-        imgAc.onload = function(){
+      let x = 0, y = 0, l = 0, t = 0, maxH = 0, maxW = 0;
+      let isDown = false;
+      imgAc.onload = function(e){
+        let h = (600 * imgAc.naturalHeight) / imgAc.naturalWidth;
+        imgAc.style.width = '100%'
+        imgAc.style.height = h + 'px';
+        iamgeShow.style.width = '600px'
+        iamgeShow.style.height = h + 'px';
+        sendImgShow.style.width = '600px'
+        sendImgShow.style.height = h + 'px';
+        maxH = imgAc.offsetHeight
+        maxW = imgAc.offsetWidth
+      };
+
+      dv.onmousedown = function(e) {
+        x = e.clientX;
+        y = e.clientY;
+        l = dv.offsetLeft;
+        t = dv.offsetTop;
+        isDown = true;
+      }
+
+      dv.onmousemove = function(e) {
+        if (isDown == false) {
+          return false;
+        } else {
+          chang(e);
+        }
+      }
+
+      dv.onmouseup = function() {
+        isDown = false;
+      }
+
+      function chang(e) {
+
+        let nx = e.clientX;
+        let ny = e.clientY;
+        //计算移动后的左偏移量和顶部的偏移量
+        var nl = nx - (x - l);
+        var nt = ny - (y - t);
+
+        if (nl > 0 && nl < 300 && nl < maxW-300) {
+          dv.style.left = nl + 'px';
+          iamgeShow.style.marginLeft =  -nl + 'px';
+          sendImgShow.style.marginLeft =  -nl + 'px';
+        }
+        if (nt > 0 &&  nt < maxH - 200 && nt < 300) {
+          dv.style.top = nt + 'px';
+          iamgeShow.style.marginTop =  -nt + 'px';
+          sendImgShow.style.marginTop =  -nt + 'px';
+        }
+      };
+
+      content.onmousewheel = function(event) {
+        let b = imgAc.offsetWidth/imgAc.offsetHeight;
+        if (event.wheelDelta > 0) {
+          iamgeShow.style.height = imgAc.offsetHeight + 15 + 'px'
+          iamgeShow.style.width = imgAc.offsetWidth + 15*b + 'px'
+          sendImgShow.style.height = imgAc.offsetHeight + 15 + 'px'
+          sendImgShow.style.width = imgAc.offsetWidth + 15*b + 'px'
+          imgAc.style.height = imgAc.offsetHeight + 15 + 'px'
+          imgAc.style.width = imgAc.offsetWidth + 15*b + 'px'
           maxH = imgAc.offsetHeight
           maxW = imgAc.offsetWidth
-        };
-
-        dv.onmousedown = function(e) {
-          x = e.clientX;
-          y = e.clientY;
-          l = dv.offsetLeft;
-          t = dv.offsetTop;
-          isDown = true;
+        } else if (event.wheelDelta < 0) {
+          if (imgAc.offsetHeight < 210 || imgAc.offsetWidth < 310 || imgAc.offsetWidth  < (dv.offsetLeft + 310) ||  imgAc.offsetHeight  < (dv.offsetTop + 220)) {
+            return false
+          }
+          iamgeShow.style.height = imgAc.offsetHeight - 15 + 'px'
+          iamgeShow.style.width = imgAc.offsetWidth - 15*b + 'px'
+          sendImgShow.style.height = imgAc.offsetHeight - 15 + 'px'
+          sendImgShow.style.width = imgAc.offsetWidth - 15*b + 'px'
+          imgAc.style.height = imgAc.offsetHeight - 15 + 'px'
+          imgAc.style.width = imgAc.offsetWidth - 15*b + 'px'
+          maxH = imgAc.offsetHeight
+          maxW = imgAc.offsetWidth
+        } else {
+          return;
         }
-
-        dv.onmousemove = function(e) {
-          if (isDown == false) {
-            return false;
-          } else {
-            chang(e);
-          }
-        }
-
-        dv.onmouseup = function() {
-          isDown = false;
-        }
-
-        function chang(e) {
-
-          let nx = e.clientX;
-          let ny = e.clientY;
-          //计算移动后的左偏移量和顶部的偏移量
-          var nl = nx - (x - l);
-          var nt = ny - (y - t);
-
-          if (nl > 0 && nl < 300 && nl < maxW-300) {
-            dv.style.left = nl + 'px';
-            iamgeShow.style.marginLeft =  -nl + 'px';
-            sendImgShow.style.marginLeft =  -nl + 'px';
-          }
-          if (nt > 0 &&  nt < maxH - 200 && nt < 300) {
-            dv.style.top = nt + 'px';
-            iamgeShow.style.marginTop =  -nt + 'px';
-            sendImgShow.style.marginTop =  -nt + 'px';
-          }
-        };
-
-        content.onmousewheel = function(event) {
-          let b = imgAc.offsetWidth/imgAc.offsetHeight;
-          if (event.wheelDelta > 0) {
-            iamgeShow.style.height = imgAc.offsetHeight + 15 + 'px'
-            iamgeShow.style.width = imgAc.offsetWidth + 15*b + 'px'
-            sendImgShow.style.height = imgAc.offsetHeight + 15 + 'px'
-            sendImgShow.style.width = imgAc.offsetWidth + 15*b + 'px'
-            imgAc.style.height = imgAc.offsetHeight + 15 + 'px'
-            imgAc.style.width = imgAc.offsetWidth + 15*b + 'px'
-            imgAc.style.marginLeft = imgAc.offsetLeft - 7.5 + 'px'
-            maxH = imgAc.offsetHeight
-            maxW = imgAc.offsetWidth
-          } else if (event.wheelDelta < 0) {
-            if (imgAc.offsetHeight < 210 || imgAc.offsetWidth < 310 || imgAc.offsetWidth  < (dv.offsetLeft + 320) ||  imgAc.offsetHeight  < (dv.offsetTop + 220)) {
-              return false
-            }
-            iamgeShow.style.height = imgAc.offsetHeight - 15 + 'px'
-            iamgeShow.style.width = imgAc.offsetWidth - 15*b + 'px'
-            sendImgShow.style.height = imgAc.offsetHeight - 15 + 'px'
-            sendImgShow.style.width = imgAc.offsetWidth - 15*b + 'px'
-            imgAc.style.height = imgAc.offsetHeight - 15 + 'px'
-            imgAc.style.width = imgAc.offsetWidth - 15*b + 'px'
-            imgAc.style.marginLeft = imgAc.offsetLeft + 7.5 + 'px'
-            maxH = imgAc.offsetHeight
-            maxW = imgAc.offsetWidth
-          } else {
-            return;
-          }
-        };
+      };
+    },
+    methods: {
+      upload() {
       },
-      methods: {
-        upload() {
-        },
-        getMessage() {
-          document.getElementById('fileElem').click();
-        },
-        handleFiles() {
-          let file = document.getElementById('fileElem');
-          let reader = new FileReader();
-          reader.readAsDataURL(file.files[0]);
-          reader.onload = (e) => {
-            this.img = e.target.result;
-            let imgAc = document.getElementById('imgAc');
-            imgAc.style.width = '100%';
-          }
-        },
+      getMessage() {
+        document.getElementById('fileElem').click();
       },
-    }
+      handleFiles() {
+        let file = document.getElementById('fileElem');
+        let reader = new FileReader();
+        reader.readAsDataURL(file.files[0]);
+        reader.onload = (e) => {
+          this.img = e.target.result;
+        }
+      },
+    },
+  }
 </script>
 
 <style scoped>
